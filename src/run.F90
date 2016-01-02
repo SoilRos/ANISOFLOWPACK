3e23c51e509786c45b0c9d029e7b94ca513d1578
soilros <soil19@hotmail.com>
2016-01-02 17:01:43 -0500

    La matriz ahora se crea en PETSc y en Array, por ahora se imprimen ambas para comparar.

--- src/run.F90
+++ src/run.F90
@@ -14,48 +14,50 @@ PROGRAM run
     !
     IMPLICIT NONE   
 #include <petsc/finclude/petscsys.h>
-! Declaracion de variables de petsc
+#include <petsc/finclude/petscmat.h>
+    ! Declaracion de variables de petsc
     PetscErrorCode                              :: ierr
     PetscMPIInt                                 :: tprocess, iprocess
-! Declaracion de variables tipo
+    Mat                                         :: pTeq
+    ! Declaracion de variables tipo
     TYPE(domain)                                :: dmngmtry
     TYPE(topology)                              :: topo
-! Declaracion de variables tipo caracter
+    ! Declaracion de variables tipo caracter
     CHARACTER(200)                              :: prjctrt,prjctrtd,route,rt
     CHARACTER(20)                               :: flnm,forminI,forminR
     CHARACTER(1) :: intro=' '  
     CHARACTER(8)::prjctnm
-! Declaracion de constantes
+    ! Declaracion de constantes
     INTEGER(I4B)                                :: i,j,k,cont,vl,aux,u,n
     REAL(DP)                                    :: xi,yi,zi,xf,yf,zf,dx,dy,dz
-! Declaracion de arreglos
+    ! Declaracion de arreglos
     REAL(DP), DIMENSION(:), ALLOCATABLE         :: X,Y,Z,kxx,kyy,kzz,kxy,kxz,kyz,qhext
     INTEGER(I4B), DIMENSION(:), ALLOCATABLE     :: topheconect
     INTEGER(I4B), DIMENSION(:,:), ALLOCATABLE   :: ijheconect
     REAL(DP), DIMENSION(:,:), ALLOCATABLE       :: Teq,qhcte
 
     ! PARAMETER(u=10)
-! Anisotropia 10
+    ! Anisotropia 10
     PARAMETER(prjctrt='../HetePP3D/Project')
     PARAMETER(prjctrtd='../HetePP3D/Data')
-! Anisotropia 20
+    ! Anisotropia 20
     ! PARAMETER(prjctrt='/cygdrive/D/Colciencias-Tesis/Validacion/Ans_40/Project')
     ! PARAMETER(prjctrtd='/cygdrive/D/Colciencias-Tesis/Validacion/Ans_40/Data')
-! Anisotropia 30
+    ! Anisotropia 30
     ! PARAMETER(prjctrt='/cygdrive/D/Colciencias-Tesis/Validacion/Ans_40/Project')
     ! PARAMETER(prjctrtd='/cygdrive/D/Colciencias-Tesis/Validacion/Ans_40/Data')
-! Anisotropia 40
+    ! Anisotropia 40
     ! PARAMETER(prjctrt='/cygdrive/D/Colciencias-Tesis/Validacion/Ans_40/Project')
     ! PARAMETER(prjctrtd='/cygdrive/D/Colciencias-Tesis/Validacion/Ans_40/Data')
-! isotropia
+    ! isotropia
     ! PARAMETER(prjctrt='/cygdrive/D/Colciencias-Tesis/Validacion/Isotropy/Project')
     ! PARAMETER(prjctrtd='/cygdrive/D/Colciencias-Tesis/Validacion/Isotropy/Data')
 
-! Inicialización del PETSc y de paralelismos.
-! iprocess: Identidicador de procesador
-!           maestro==0
-!           esclavo/=0
-! tprocess: Cantidad total de procesadores en funcionamiento.
+    ! Inicialización del PETSc y de paralelismos.
+    ! iprocess: Identidicador de procesador
+    !           maestro==0
+    !           esclavo/=0
+    ! tprocess: Cantidad total de procesadores en funcionamiento.
     CALL PetscInitialize(PETSC_COMM_WORLD,ierr) 
     CALL MPI_comm_rank(MPI_COMM_WORLD,iprocess,ierr)
     CALL MPI_comm_size(MPI_COMM_WORLD,tprocess,ierr)
@@ -73,22 +75,26 @@ PROGRAM run
     END IF
 
     CALL bld_topologybn(prjctrt,dmngmtry,topo)   
-
+    !Construcción de matriz en Petsc (Recordar que los indices de Petsc inician en 0)
+    CALL ensmblT_bn(prjctrt,topo,dmngmtry,pTeq,topheconect,ijheconect,qhcte,qhext)
+    !
     IF (iprocess==0) THEN
-        CALL isoTdns_bn(prjctrt,topo,dmngmtry,Teq,topheconect,ijheconect,qhcte,qhext)
+        ! Construcción de matriz en array (Recordar que los indices de FORTRAN inician en 1)
+        CALL ensmblT_bn(prjctrt,topo,dmngmtry,Teq,topheconect,ijheconect,qhcte,qhext)
+        !
         ! aux=dmngmtry%clmns*dmngmtry%rws*dmngmtry%lvls
         ! rt=trim(prjctrt)//'/sanpck.alf'
         ! OPEN(u, FILE=rt, STATUS='OLD', ACCESS='DIRECT', RECL=18*8, FORM='UNFORMATTED')
         ! CALL gtvlr_bn(u,dmngmtry,2,2,2,vl) 
         
-! Bucle para imprimir matriz densa
-        ! do i=topo%act/2,topo%act
-            ! do j=topo%act/2,topo%act
-            !     write(*,*)Teq(i,j),i,j
-            ! end do
-        ! end do
+        ! Bucle para imprimir matriz densa
+        do i=topo%act/2,topo%act
+            do j=topo%act/2,topo%act
+                write(*,*)Teq(i,j),i,j
+            end do
+        end do
         
-! UTIL PARA ABRIR UN ARCHIVO Y LEER
+        ! UTIL PARA ABRIR UN ARCHIVO Y LEER
         ! rt=trim(prjctrt)//'/sanpck.kxx'
         ! OPEN(u, FILE=rt, STATUS='OLD', ACCESS='DIRECT', RECL=18*8, FORM='UNFORMATTED')
         ! n=dmngmtry%lvls*dmngmtry%rws*dmngmtry%clmns
